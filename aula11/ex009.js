f2294d3894aadc9151b48772a4cc4a00bc7a1a5e
gabrielfsmiranda <gabrielfsmiranda@gmail.com>
2025-02-05 18:45:02 -0300

    exercicios 8-10

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo no seguinte país: ${país}`)
+if (país != 'Brasil') {
+    console.log('Você é Estrangeiro.')
+} else {
+    console.log('Você é Brasileiro.')
+}
