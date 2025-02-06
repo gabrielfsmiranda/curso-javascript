242917786e6bcb5848a21575f87def8bc8244768
gabrielfsmiranda <gabrielfsmiranda@gmail.com>
2025-02-06 14:36:40 -0300

    inicio repeticoes parte 2

---
+++ aula14/ambiente.js
@@ -0,0 +1,28 @@
+/*
+var c = 1
+
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+*/
+
+/*
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+
+/*
+for (var c = 1; c <=10; c++) { //na variavel do meio que vc limita
+    console.log(c)
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1; c <=4; c++) { //na variavel do meio que vc limita
+    console.log(c)
+}
+console.log('Fim!')
+
