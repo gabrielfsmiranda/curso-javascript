a7500f1be8e4b2f8dfe6b6c3d47ed185796b5439
gabrielfsmiranda <gabrielfsmiranda@gmail.com>
2025-02-06 13:21:42 -0300

    aula 13 curso js - repetições 1

---
+++ aula13/ambiente.js
@@ -0,0 +1,52 @@
+/*
+var fatias = 8
+
+do {
+    console.log(`Restam ${fatias} fatias`)
+    fatias --
+} while (fatias >= 0)
+
+Finalmente consegui fazer a contagem de fatias hehehe
+
+*/
+
+
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c ++ // c = c + 1
+} while (c <= 7)
+
+
+
+/*
+
+var c = 1
+
+while (c <= 14) { //aqui vc limita os passos.. teste e veja!
+    console.log(`Passo ${c}`)
+    c ++ // c = c + 1
+}
+
+*/
+
+
+
+/*
+var c = 1
+
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c ++ // c = c + 1
+}
+
+Isso de baixo tem o msm resultado do de cima
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
