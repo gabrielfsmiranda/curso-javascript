bf98c827b4521d0616ab8ba416f9d794e9726c0a
gabrielfsmiranda <gabrielfsmiranda@gmail.com>
2025-02-06 14:49:26 -0300

    aula 14 finalizada

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -20,6 +20,12 @@ for (var c = 1; c <=10; c++) { //na variavel do meio que vc limita
 }
 */
 
+/*
+Vimos debug console (F8) com o codigo abaixo
+adiciona variavel pra observar, clica no começo ao lado da linha de codigo, vai apertando F10... tem que selecionar o certo la em cima na aba de debug tb (Run Current File)
+*/
+
+
 console.log('Vai começar...')
 for (var c = 1; c <=4; c++) { //na variavel do meio que vc limita
     console.log(c)
