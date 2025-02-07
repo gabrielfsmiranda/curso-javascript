e6c12ecce25bafafb5d5afa01989fee447c24d51
gabrielfsmiranda <gabrielfsmiranda@gmail.com>
2025-02-07 00:54:14 -0300

    fiz a tabuada por conta propria

--- aula14/ex17-2/script.js
+++ aula14/ex17-2/script.js
@@ -1,1 +1,16 @@
-alert('Olá')
+function calcular() {
+    let num = document.getElementById('inum')
+    let mult = 1
+    let n = Number(num.value)
+    let tab = document.getElementById('itab')
+    tab.innerHTML = ''
+
+    while (mult <= 10) {
+        let res = document.createElement('option')
+        res.text = `${n} x ${mult} = ${n * mult}`
+    //    res.value = `opt${mult}` opcional p php
+        tab.appendChild(res)
+        mult ++
+    }
+
+}
