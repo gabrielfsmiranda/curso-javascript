f57a5b9633fde17a9ee954d40a679ed12da96280
gabrielfsmiranda <gabrielfsmiranda@gmail.com>
2025-02-11 00:28:32 -0300

    treino

---
+++ modelo/script.js
@@ -0,0 +1,31 @@
+/*
+const person = {};
+person.name = 'Gabriel';
+person.surname = 'Miranda';
+person.fullname = function(){
+    return this.name + ' ' + this.surname
+};
+
+console.log(person.fullname());
+console.log(person.fullname)
+*/
+
+/*
+const fruits = {Bananas:300, Oranges:200, Apples:500}; 
+
+let text = "";
+for (let [fruta, qual] of Object.entries(fruits)) {
+  text += fruta + ": " + qual + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+*/
+
+// não importa os nomes q vc coloque apos let [x,y], contanto q sejam 2 nomes.. essa função vai fazer loop q percorre todo objeto pegando propriedade e valor...
+
+/*
+const fruits = {Bananas:300, Oranges:200, Apples:500}; 
+alert(Object.values(fruits))
+
+esse só mostra os valores
+*/
