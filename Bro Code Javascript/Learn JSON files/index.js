ea835f6bbbb84167c8b4116f336b4a5ceaed6d9e
gabrielfsmiranda <gabrielfsmiranda@gmail.com>
2025-02-24 20:51:58 -0300

    Learning JSON files

---
+++ Bro Code Javascript/Learn JSON files/index.js
@@ -0,0 +1,72 @@
+// JSON = (JavaScript Object Notation) data-interchange format 
+//        Used for exchanging data between a server and a web application
+//        JSON files {key:value} OR [value1, value2, value3]
+
+//        JSON.stringify() = converts a JS object to a JSON string.
+//        JSON.parse() = converts a JSON string to a JS object
+
+// ---------- JSON.stringify() ----------
+
+const names = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const person = {
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["Jellyfishing", "Karate", "Cooking"]
+};
+const people = [{
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true
+},
+{
+    "name": "Patrick",
+    "age": 34,
+    "isEmployed": false
+},
+{
+    "name": "Squidward",
+    "age": 50,
+    "isEmployed": true
+},
+{
+    "name": "Sandy",
+    "age": 27,
+    "isEmployed": false
+}];
+
+const jsonString = JSON.stringify(people);
+
+console.log(jsonString);
+
+// resultado -> string com o JSON por extenso
+
+
+
+
+// ---------- JSON.parse() ----------
+
+const jsonNames = `["Spongebob", "Patrick", "Squidward", "Sandy"]`;
+const jsonPerson = `{"name": "Spongebob", "age": 30, "isEmployed": true, "hobbies": ["Jellyfishing", "Karate", "Cooking"]}`;
+const jsonPeople = `[{"name": "Spongebob","age": 30,"isEmployed": true},
+                                     {"name": "Patrick","age": 34,"isEmployed": false},
+                                     {"name": "Squidward","age": 50,"isEmployed": true},
+                                     {"name": "Sandy","age": 27,"isEmployed": false}]`;
+
+const parsedData = JSON.parse(jsonPeople);
+
+console.log(parsedData);
+
+// pega a string em formato JSON e transforma em objeto JS (array ou objeto)
+
+
+
+// ---------- fetch() ----------
+
+fetch("people.json")
+    .then(response => response.json())
+    .then(values => values.forEach(value => console.log(value)))
+    .catch(error => console.error(error));
+
+// pega JSON de arquivo externo
+// ver chat salvo com o chat gpt onde me ensina a salvar JSON com o codigo JS!!!
